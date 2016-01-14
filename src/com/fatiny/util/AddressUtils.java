8a5b5af0cd93b0ba96834b2e256c6b15aae488c4
Jeremy <361600134@qq.com>
2016-01-14 21:57:25 +0800

    一根文件

--- src/com/fatiny/util/AddressUtils.java
+++ src/com/fatiny/util/AddressUtils.java
@@ -4,25 +4,34 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Test;
 import com.maxmind.geoip.Location;
 import com.maxmind.geoip.LookupService;
 import com.maxmind.geoip.regionName;
+
 
 /**
  * 根据IP地址获取详细的地域信息
  */
 public class AddressUtils {
 	
-	private static final String localhost = "Local or LAN";
+	private static final String localhost = "本地地址";
 	private static final String localip = "192.168.1.";
 	private static final String localip2 = "0:0:0:0:0:0:0:1";
 	private static final String errorResult = "无效IP或未解析成功";
@@ -43,89 +52,72 @@ public class AddressUtils {
 	}
 	
 	/**
-	 * 获取指定IP的区域位置
-	 * @param content
-	 *            请求的参数 格式为：name=xxx&pwd=xxx
-	 * @param encoding
-	 *            服务器端请求编码。如GBK,UTF-8等
-	 * @return
-	 * @throws UnsupportedEncodingException
-	 */
-	public static String getAddresses(String ip) {
-		// 这里调用pconline的接口
-		String urlStr = "http://ip.taobao.com/service/getIpInfo.php";
-		String content = "ip="+ip;
-		// 从http://whois.pconline.com.cn取得IP所在的省市区信息
-		String returnStr = getResult(urlStr, content);
-		if (returnStr != null) {
-			// 处理返回的省市区信息
-			//System.out.println(returnStr);
-			String[] temp = returnStr.split(",");
-			if (temp.length < 3) {
-				return "0";// 无效IP，局域网测试
+	    * 方法名：getIpInfo</br>
+	    * 详述：通过IP获取地址(需要联网，调用淘宝的IP库)</br>
+	    * 开发人员：souvc </br>
+	    * 创建时间：2015-11-10  </br>
+	    * @param ip
+	    * @return
+	    * @throws
+	     */
+	    public static String getIpInfo(String ip) {
+	    	//本地
+			if (ip.contains(localip) || ip.contains(localip2)) {
+				return localhost;
 			}
-			String contry = (temp[2].split(":"))[1].replaceAll("\"", "");
-			String area = (temp[3].split(":"))[1].replaceAll("\"", "");
-			String region = (temp[5].split(":"))[1].replaceAll("\"", "");
-			String city = (temp[7].split(":"))[1].replaceAll("\"", "");
-			String isp = (temp[11].split(":"))[1].replaceAll("\"", "");
-			area = decodeUnicode(area); //地区
-			contry = decodeUnicode(contry); //国家
-			region = decodeUnicode(region);// 省份
-			city = decodeUnicode(city);// 省份
-			isp = decodeUnicode(isp);// 公司
-			return region + city + isp;
-		}
-		return errorResult;
-	}
-
-	/**
-	 * @param urlStr
-	 *            请求的地址
-	 * @param content
-	 *            请求的参数 格式为：name=xxx&pwd=xxx
-	 * @param encoding
-	 *            服务器端请求编码。如GBK,UTF-8等
-	 * @return
-	 */
-	private static String getResult(String urlStr, String content) {
-		URL url = null;
-		HttpURLConnection connection = null;
-		try {
-			url = new URL(urlStr);
-			connection = (HttpURLConnection) url.openConnection();// 新建连接实例
-			connection.setConnectTimeout(2000);// 设置连接超时时间，单位毫秒
-			connection.setReadTimeout(2000);// 设置读取数据超时时间，单位毫秒
-			connection.setDoOutput(true);// 是否打开输出流 true|false
-			connection.setDoInput(true);// 是否打开输入流true|false
-			connection.setRequestMethod("POST");// 提交方法POST|GET
-			connection.setUseCaches(false);// 是否缓存true|false
-			connection.connect();// 打开连接端口
-			DataOutputStream out = new DataOutputStream(
-					connection.getOutputStream());// 打开输出流往对端服务器写数据
-			out.writeBytes(content);// 写数据,也就是提交你的表单 name=xxx&pwd=xxx
-			out.flush();// 刷新
-			out.close();// 关闭输出流
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					connection.getInputStream(), "utf-8"));// 往对端写完数据对端服务器返回数据
-			// ,以BufferedReader流来读取
-			StringBuffer buffer = new StringBuffer();
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				buffer.append(line);
-			}
-			reader.close();
-			return buffer.toString();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (connection != null) {
-				connection.disconnect();// 关闭连接
-			}
-		}
-		return null;
-	}
-
+	        String info = "";
+	        try {
+	            URL url = new URL("http://ip.taobao.com/service/getIpInfo.php?ip=" + ip);
+	            HttpURLConnection htpcon = (HttpURLConnection) url.openConnection();
+	            htpcon.setRequestMethod("GET");
+	            htpcon.setDoOutput(true);
+	            htpcon.setDoInput(true);
+	            htpcon.setUseCaches(false);
+	 
+	            InputStream in = htpcon.getInputStream();
+	            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+	            StringBuffer returnStr = new StringBuffer();
+	            String line = bufferedReader.readLine();
+	            while (line != null) {
+	            	returnStr.append(line).append("\r\n");
+	                line = bufferedReader.readLine();
+	            }
+	            bufferedReader.close();
+	            
+	            if (returnStr != null) {
+	    			// 处理返回的省市区信息
+	    			//System.out.println(returnStr);
+	    			String[] temp = returnStr.toString().split(",");
+	    			if (temp.length < 3) {
+	    				return "0";// 无效IP，局域网测试
+	    			}
+	    			String contry = (temp[2].split(":"))[1].replaceAll("\"", "");
+	    			String area = (temp[3].split(":"))[1].replaceAll("\"", "");
+	    			String region = (temp[5].split(":"))[1].replaceAll("\"", "");
+	    			String city = (temp[7].split(":"))[1].replaceAll("\"", "");
+	    			String isp = (temp[11].split(":"))[1].replaceAll("\"", "");
+	    			area = decodeUnicode(area); //地区
+	    			contry = decodeUnicode(contry); //国家
+	    			region = decodeUnicode(region);// 省份
+	    			city = decodeUnicode(city);// 省份
+	    			isp = decodeUnicode(isp);// 公司
+	    			
+	    			if (region == "" || region.equals(""))
+						return contry;
+	    			
+	    			return area + region + city + isp;
+	    		}
+	            
+	        } catch (MalformedURLException e) {
+	            e.printStackTrace();
+	        } catch (ProtocolException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        return info;
+	    }
+	
 	/**
 	 * unicode 转换成 中文
 	 * @author fanhui 2007-3-15
@@ -197,7 +189,7 @@ public class AddressUtils {
 		}
 		return outBuffer.toString();
 	}
-
+	
 	public static String getGeoAddress(String ip){
 		//本地
 		if (ip.contains(localip) || ip.contains(localip2)) {
@@ -229,21 +221,46 @@ public class AddressUtils {
 		return errorResult;
 	}
 	
-	
-	// 测试
-	public static void main(String[] args) {
-		// 测试ip 219.136.134.157 中国=华南=广东省=广州市=越秀区=电信
-		// 测试ip 125.70.11.136 中国=西南=四川省=成都市==电信
-		//String ip = "218.192.3.42";
-		//String ip="112.74.85.106";	//广东深圳
-		//ip = "103.9.116.204";	//台湾省
-		//ip = "119.81.157.66";	//HK
-		//ip = "5.39.51.137";		//FR
-		//ip = "24.143.198.188";	//US
-		List<String> list = new ArrayList<String>(Arrays.asList("103.9.116.204", "112.74.85.106"));
+	@Test
+	public void splitips(){
+		List<String> list = new ArrayList<String>(Arrays.asList("219.136.134.157", "112.74.85.106", "119.81.157.66", "24.143.198.188"));
 		for (String str : list) {
-			String address = getAddresses(str);
+			String address = getIpInfo(str);
 			System.out.println("==>"+address);
 		}
 	}
+	
+	@Test
+	public void splitGeoip(){
+//		 测试ip 219.136.134.157 中国=华南=广东省=广州市=越秀区=电信
+//		 测试ip 125.70.11.136 中国=西南=四川省=成都市==电信
+		String ip = "117.150.35.41";
+//		String ip="112.74.85.106";	//广东深圳
+//		ip = "103.9.116.204";	//台湾省
+//		ip = "119.81.157.66";	//HK
+//		ip = "5.39.51.137";		//FR
+//		ip = "24.143.198.188";	//US
+		String address = getGeoAddress(ip);
+		System.out.println("==>"+address);
+	}
+	
+	@Test
+	public void splitGeoips(){
+//		List<String> list = new ArrayList<String>(Arrays.asList("103.9.116.204", "24.143.198.188"));
+		List<String> ipList = new ArrayList<String>();
+		for (int i = 1; i <= 2000; i++) {
+			ipList.add(("119.81.157."+i));
+		}
+		ipList.add("219.136.134.157");
+		ipList.add("103.9.116.204");
+//		Arrays.asList("103.9.116.204", "24.143.198.188")
+		long beginTime = System.currentTimeMillis();
+		for (String str : ipList) {
+			String address = getGeoAddress(str);
+			System.out.println("==>"+address);
+		}
+		System.out.println("use time: "+(System.currentTimeMillis() - beginTime));
+	}
+	
+	
 }
